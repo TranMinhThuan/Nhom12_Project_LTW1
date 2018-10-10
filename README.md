e97d788843dc1df0b3cbc9e84f05934302d1fa07
Student <Student@tdc.edu.vn>
2018-10-10 15:49:38 +0700

    thanh

--- README.md
+++
@@ -1,2 +0,0 @@
-# Nhom12_Project_LTW1
-Đồ án lập trình web 1
--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "config.php";
-	require "db.php";
-	$ID = $_GET['ID'];
-	$db = new Db;
-
-	$delete = $db->delete($ID);
-	header('location:index.php');
-?>
